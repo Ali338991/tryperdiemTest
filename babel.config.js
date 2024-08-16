de5954d6d237d53abf45fd9f9eeb88a309fa88f3
Ali338991 <ali.ansari7268@gmail.com>
2024-08-16 08:42:49 +0500

    feat: initilize projct and add libraries

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets': './assets',
+          '@store': './src/redux',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@app': './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: 'react-native-dotenv',
+        path: '.env',
+      },
+    ],
+  ],
 };
